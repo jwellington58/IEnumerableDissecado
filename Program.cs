2e4bf9bf7bcbc02d300bc03036722f55d55454bd
jwellington58 <wj070996@gmail.com>
2022-02-09 22:02:14 -0300

    starting project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+foreach(var i in RetornaDe1a10())
+{
+    Console.WriteLine(i);
+}
+
+
+static IEnumerable<int> RetornaDe1a10()
+{
+    yield return 1;
+    yield return 2;
+    yield return 3;
+    yield return 4;
+    yield return 5;
+    yield return 6;
+    yield return 7;
+    yield return 8;
+    yield return 9;
+    yield return 10;
+}
